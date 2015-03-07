dc7914148ca5d0141a63341a008fce8407bab552
Jose Teodoro Celaya-Alcala <jtcelayaalcala@email.arizona.edu>
2015-03-07 05:48:16 -0700

    Revert "Refactored name"
    This reverts commit 083e73b613a2e70b2618e8a9a4afd5ea456dd3b0.
    
    "Reverting"

--- src/model/Racket.java
+++ src/model/Racket.java
@@ -3,11 +3,11 @@ package model;
 public class Racket extends Entity {
 	public static enum Orient { VERTICAL, HORIZONTAL }
 	public static final int L_DEFAULT = 5;
-	private int length;
+	private int l;
 
 	public Racket(int x, int y, int l, Orient o) {
 		super(x,y);
-		this.length = l;
+		this.l = l;
 		switch(o) {
 		case VERTICAL:
 			setH(l);
@@ -33,5 +33,5 @@ public class Racket extends Entity {
 		this(0,0);
 	}
 	
-	public int getL() { return length; }
+	public int getL() { return l; }
 }
